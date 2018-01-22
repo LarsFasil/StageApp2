451a8d27b49e632365d21e7ab6bff517c8415c4d
fasill001 <lars.fasil@hva.nl>
2018-01-22 16:55:36 +0100

    singlecubescript aangemaakt

---
+++ Techniek opdracht/Assets/Scripts/SingleCubeScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleCubeScript : MonoBehaviour {
+    private MeshRenderer[] boxMeshes;
+
+	// Use this for initialization
+	void Start () {
+		
+        //elke cubemesh uit de scene pakken en bewaren in een array
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
